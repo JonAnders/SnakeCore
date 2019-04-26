c0aade7cb7b5ec09bd55c7b1aeca5c489c0839a9
Jon Anders Amundsen <jon-anders.amundsen@payex.com>
2019-04-26 10:51:27 +0200

    Set up basic BattleSnake API skeleton

---
+++ SnakeCore.Web/Controllers/BattleSnakeController.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SnakeCore.Web.Controllers
+{
+    [Route("{brain}")]
+    [ApiController]
+    public class BattleSnakeController : Controller
+    {
+        private readonly ILogger logger;
+
+
+        public BattleSnakeController(ILogger<BattleSnakeController> logger)
+        {
+            this.logger = logger;
+        }
+
+
+        [Route("start")]
+        public IActionResult Start()
+        {
+            return Json(new
+            {
+                Color = "#aaaaaa",
+                HeadType = "bendr",
+                TailType = "pixel"
+            });
+        }
+        
+
+        [HttpPost("move")]
+        public IActionResult Move()
+        {
+            var jsonBody = new StreamReader(Request.Body).ReadToEnd();
+
+            this.logger.LogDebug(jsonBody);
+
+            return Json(new
+            {
+                Move = "left"
+            });
+        }
+
+        
+        [HttpPost("end")]
+        public IActionResult End()
+        {
+            return Ok();
+        }
+        
+
+        [Route("ping")]
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
